035a9e09d3a17d15f52b84780792be88ade4fb23
Azizbek Tojimakhammatov <122020431+Tojimakhammatov@users.noreply.github.com>
2023-01-05 21:26:31 +0500

    create subfolder and python scripts

---
+++ python-code/numbers.py
@@ -0,0 +1,36 @@
+
+x, y, z = 11, -7, 82
+print(x, y, z)
+
+ism = "Ahmad"
+yosh = 41
+xabar = ism + ' ' + str(yosh) + " yoshda"
+print(xabar)
+print(type(ism))
+
+t_yil = int(input("Tugilgan yilingizni kiriting : "))
+yosh = 2022 - t_yil
+print("Siz ", yosh, " yoshda ekansiz.")
+
+a= int("10")    # int - integer, butun son
+b = float("10") # floating point numbers (O`nlik sonlar)
+temp = str(36.6) # str - matn 
+print(a, b, temp)
+
+# Homework
+
+a = int(input("Istalgan sonni kiriting \n >>> "))
+print(a, " ning kvadrati", a**2, " ga teng")
+print(a, " ning kubi", a**3, " ga teng")
+
+a = int(input("Yoshingiz nechida? \n >>> "))
+b = 2022 - a
+print("Siz", b, "- yilda tug`ilgansiz.")
+
+a = float(input("Birinchi sonni kiriting \n >> "))
+b = float(input("Ikkinchi sonni kiriting \n >> "))
+print("a + b =", a+b)
+print("a - b =", a-b)
+print("a x b =", a*b)
+print("a / b =", a/b)
+
